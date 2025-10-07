fd92d8b11da82a426bea0887a0618d890402726f
GuillermoOM <guille8ameou@gmail.com>
2025-10-06 23:08:30 -0700

    fix

--- init.lua
+++ init.lua
@@ -199,12 +199,15 @@ vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper win
 -- vim.keymap.set("n", "<C-S-l>", "<C-w>L", { desc = "Move window to the right" })
 -- vim.keymap.set("n", "<C-S-j>", "<C-w>J", { desc = "Move window to the lower" })
 -- vim.keymap.set("n", "<C-S-k>", "<C-w>K", { desc = "Move window to the upper" })
+--
 vim.filetype.add {
   extension = {
     ['ship'] = 'lua',
     ['wepn'] = 'lua',
     ['miss'] = 'lua',
     ['wf'] = 'lua',
+    ['subs'] = 'lua',
+    ['event'] = 'lua',
   },
 }
 -- [[ Basic Autocommands ]]
@@ -683,7 +686,7 @@ require('lazy').setup({
         -- clangd = {},
         -- gopls = {},
         pyright = {},
-        rome = {},
+        biome = {},
         ts_ls = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
